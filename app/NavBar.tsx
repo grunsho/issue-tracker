13ad51885621329ee72b9c5f26e60d4f0bc0c542
Álvaro Manterola <alvaro.manterola@gmail.com>
2024-03-24 03:48:47 -0300

    Build the new issue page

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,10 +1,10 @@
-"use client"
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React from 'react'
-import { AiFillBug } from "react-icons/ai"
-import classNames from 'classnames'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
 
 const NavBar = () => {
     const currentPath = usePathname();
@@ -12,27 +12,29 @@ const NavBar = () => {
     const links = [
         { label: "Dashboard", href: "/" },
         { label: "Issues", href: "/issues" },
-    ]
+    ];
     return (
         <nav className="flex items-center space-x-6 border-b mb-5 px-5 h-14">
-            <Link href="/"><AiFillBug></AiFillBug></Link>
-            <ul className='flex space-x-6'>
-                {links.map(link =>
+            <Link href="/">
+                <AiFillBug></AiFillBug>
+            </Link>
+            <ul className="flex space-x-6">
+                {links.map((link) => (
                     <Link
                         key={link.href}
                         href={link.href}
                         className={classNames({
-                            'text-zinc-900': link.href === currentPath,
-                            'text-zinc-500': link.href !== currentPath,
-                            'hover:text-zinc-800 transition-colors': true
+                            "text-zinc-900": link.href === currentPath,
+                            "text-zinc-500": link.href !== currentPath,
+                            "hover:text-zinc-800 transition-colors": true,
                         })}
                     >
                         {link.label}
                     </Link>
-                )}
+                ))}
             </ul>
         </nav>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
